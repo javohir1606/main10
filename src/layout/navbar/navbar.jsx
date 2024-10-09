9350a39bacd27320bc89edef607c9486056ea62b
javohir1606 <javohirallayorov1606@gmail.com>
2024-10-09 16:14:30 +0500

    main

--- src/layout/navbar/navbar.jsx
+++ src/layout/navbar/navbar.jsx
@@ -6,9 +6,9 @@ export const Navbar = () => {
       <div className=" bg-[#0E1821] py-[22px]">
         <div className="container flex gap-[20px]">
         <Link className="text-[#fff] font-medium text-[16px]" to={"/"}>Каталог товаров</Link>
-        <Link className="text-[#fff] font-medium text-[16px]" to={"/about"}>О компании</Link>
+        <Link className="text-[#fff] font-medium text-[16px]" to={"/conponent"}>О компании</Link>
         <Link className="text-[#fff] font-medium text-[16px]" to={"/aksiya"}>Акции</Link>
-        <Link className="text-[#fff] font-medium text-[16px]" to={"/conponent"}>Рассрочка 0|0|18</Link>
+        <Link className="text-[#fff] font-medium text-[16px]" to={"/about"}>Рассрочка 0|0|18</Link>
         <Link className="text-[#fff] font-medium text-[16px]" to={"/servic"}>Сервис и гарантия</Link>
         <Link className="text-[#fff] font-medium text-[16px]" to={"/menu"}>Опт/дропшиппинг</Link>
         <Link className="text-[#fff] font-medium text-[16px]" to={"/contact"}>Контакты</Link>
--- src/pages/conpanet/conpanet.jsx
+++ src/pages/conpanet/conpanet.jsx
@@ -1,5 +1,118 @@
+import img from "../../assets/img/rectangle.png"
 export const Conpanet = () =>{
   return<>
-  <div>Conpanet</div>
+  <div className="container">
+     <div className="flex gap-[38px] mt-10 mb-[30px]">
+      <h3 className="font-normal text-[16px]">Главная</h3>
+      <p>Новости</p>
+     </div>
+      <h1 className="font-bold text-[36px] text-black mb-[30px]">Новости</h1>
+      <div className="grid grid-cols-2 items-center mb-10">
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EDF2F6] p-[30px] rounded-[8px] w-[640px] h-[239px] flex gap-5 mb-5">
+          <div className="">
+            <img src={img} alt="" />
+          </div>
+          <div className="">
+            <h2 className="mb-[15px] font-bold text-[28px] text-black">Открытие нового магазина</h2>
+            <p className="mb-[10px] w-[400px] font-normal text-[16px]">Разнообразный и богатый опыт говорит нам, что консультация с широким активом требует от нас анализа анализа существующих паттернов поведения</p>
+            <div className="flex justify-between items-center">
+            <p className="font-medium text-[16px] text-[#2a5275]">Подробнее</p>
+              <p className="font-medium text-[16px] text-gray-400">05 июня 2021</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
   </>
 }
